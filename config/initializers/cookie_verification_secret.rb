837c92a16c8ec49175fcb9c49c0a45fd02db27a5
LaSombra <lasombra@truta.org>
2010-08-03 00:20:29 -0300

    Initial commit

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'e8e79ed8b9b434b0690a78d83c10443ed49bb699a55ae5f9a9e43ddea1441039ada9bbd2b70a1486acd3f6f1c332ba5df46c6f38b79683e6aeb3246ecc8d5f96';
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_demo_app_session',
+  :secret      => '15b07f4f2fe9bb2f5cd77f76212a2648948bb211564392924328724c4a2dd13c3a32891c15fae42e403fe492442e95983b71682de469c704e0fec661395b24d0'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
